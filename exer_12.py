a9c07b8b722d350d60059f3a2cc89193a832e59d
Antoniojjr <91753417+Antoniojjr@users.noreply.github.com>
2024-07-28 15:15:47 -0300

    Update exer_12.py

--- exer_12.py
+++ exer_12.py
@@ -1,3 +1,9 @@
+"""
+12. Dado um conjunto de números inteiros informados pelo usuário, construa um programa
+que calcule a média aritmética dos números pares. O valor de finalização será a entrada do
+número 0 e não entrará nos cálculos. Observe que nada impede que o usuário forneça
+quantos números ímpares quiser, com a ressalva de que eles não entrarão nos cálculos.
+"""
 
 vetor_par = []
 total_numeros = 0
