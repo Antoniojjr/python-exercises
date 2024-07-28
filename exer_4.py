c8eddd8b2d6ee92fb52e765e4f05c396dc4d4e33
Antoniojjr <91753417+Antoniojjr@users.noreply.github.com>
2024-07-28 15:12:05 -0300

    exer_4.py

--- exer_4.py
+++ exer_4.py
@@ -1,3 +1,9 @@
+"""
+4. Ler um vetor com 10 nomes de pessoas, após pedir que o usuário digite um nome
+qualquer de pessoa. Escrever a mensagem “ACHEI”, se o nome estiver armazenado no
+vetor C ou “NÃO ACHEI” caso contrário.
+"""
+
 vetor_1 = []
 
 for _ in range(10):
