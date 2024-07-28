834ee64c1135cdfad83d474078a5cd751a33eee8
Antoniojjr <91753417+Antoniojjr@users.noreply.github.com>
2024-07-28 15:15:23 -0300

    exer_11.py

--- exer_11.py
+++ exer_11.py
@@ -1,3 +1,10 @@
+"""
+11. Faça um programa que calcule N! (fatorial de N), sendo que o valor inteiro de N é
+fornecido pelo usuário. Sabe-se que:
+
+N! = 1 * 2 * 3 * 4 * ... * (N – 1) * N; Exemplo: fatorial de 4 é 1 * 2 * 3 * 4 = 24.
+0! = 1, por definição.
+"""
 
 number = int(input('Digite um número: '))
 
