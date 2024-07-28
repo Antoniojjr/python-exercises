faca46643d227184d9861c2e176126ef491c1f58
Antoniojjr <91753417+Antoniojjr@users.noreply.github.com>
2024-07-28 15:14:26 -0300

    exer_9.py

--- exer_9.py
+++ exer_9.py
@@ -1,3 +1,11 @@
+"""
+9. Faça um programa que leia um número inteiro positivo n e imprima n linhas na tela com o
+seguinte formato (exemplo se n = 4):
+1
+1 2
+1 2 3
+1 2 3 4
+"""
 
 linha = int(input("Número de linhas: "))
 
