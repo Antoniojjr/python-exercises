a1f1406e9275455de03c0a4cb11ad83fc22fafc5
Antoniojjr <91753417+Antoniojjr@users.noreply.github.com>
2024-07-28 15:14:49 -0300

    exer_10.py

--- exer_10.py
+++ exer_10.py
@@ -1,3 +1,13 @@
+"""
+10. Escreva um programa que leia um número inteiro positivo n e em seguida imprima n
+linhas do chamado triângulo de Floyd. O exemplo abaixo mostra o triângulo de Floyd com 4
+linhas.
+1
+2 3
+4 5 6
+7 8 9 10
+"""
+
 n = int(input())
 numero = 1
 
