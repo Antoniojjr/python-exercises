2b1022c55e24b98636329cd7bfe53c0896bb2382
Antoniojjr <91753417+Antoniojjr@users.noreply.github.com>
2024-07-28 15:10:10 -0300

    exer_2.py

--- exer_2.py
+++ exer_2.py
@@ -1,3 +1,8 @@
+"""
+2. Ler um vetor de 10 elementos. Crie um segundo vetor, com todos os elementos na ordem
+inversa, ou seja, o último elemento passará a ser o primeiro, o penúltimo será o segundo e
+assim por diante. Imprima os dois vetores.
+"""
 
 vetor_1 = []
 vetor_2 = []
